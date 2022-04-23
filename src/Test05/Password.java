43ba54f6cb0aae4df201643e37d31e201efbbfc5
AidaQ27 <aidaqueraltt@gmail.com>
2022-04-24 00:16:34 +0200

    ejericicio 05 password

--- src/Test05/Password.java
+++ src/Test05/Password.java
@@ -6,6 +6,7 @@ public class Password {
 	//Atributos de la clase
 		private int longitud;
 		private String contrasenya;
+		private boolean fuerte;
 		
 
 
@@ -15,12 +16,14 @@ public class Password {
 	public Password(){
 		this.longitud = 8;
 		this.contrasenya = contrasenyaAleatoria();
+		esFuerte();
 		}
 
 	//Constructor con la longitud introducida por el constructor
 	public Password(int longitud) {
 		this.longitud = longitud;
 		this.contrasenya = contrasenyaAleatoria();
+		esFuerte();
 	}
 
 	//getters & setters
@@ -32,6 +35,10 @@ public class Password {
 		return longitud;
 	}
 
+	// Para modificar la longitud
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
 
 	// Devuelve la contraseña
 	public String getContrasenya() {
@@ -42,27 +49,74 @@ public class Password {
 	public void setContrasenya(String contrasenya) {
 		this.contrasenya = contrasenya;
 	}
+	
 
-	//Metodo para generar una contraseña aleatoria
+	public boolean isFuerte() {
+		return fuerte;
+	}
+
+	public void setFuerte(boolean fuerte) {
+		this.fuerte = fuerte;
+	}
 
 	public String contrasenyaAleatoria() {
 		String contrasenya = "";
-		final char[] numeros = {'1','2','3','4','5','6','7','8','9'};
 		
+		final char[] numeros = {'1','2','3','4','5','6','7','8','9',};
+		final char[] minisculas = {'a','b','c','d','e','f','g','h','i',};
+		final char[] mayusculas = {'A','B','C','D','E','F','G','H','I',};
 		// Creamos un nuevo objeto random
 		Random aleatoria = new Random();
 		for (int i = 0; i < longitud; i++) {
-			contrasenya += numeros[aleatoria.nextInt(numeros.length)];
-			
-		}
-		
+			int numero = (int) (Math.random() * 3) + 1;
+			if (numero == 1) {
+				contrasenya += numeros[aleatoria.nextInt(numeros.length)];		
+			}else if (numero == 2) {
+				contrasenya += minisculas[aleatoria.nextInt(numeros.length)];
+			}else{
+				contrasenya += mayusculas[aleatoria.nextInt(numeros.length)];
+				}
+			}
+	
+		//System.out.println(contrasenya);
 		return contrasenya;
 	}
+	
+	public void esFuerte() {
+	//numeros = 6, minisculas = 1, mayusculas = 2
+	int contadorNumeros = 0;
+	int contadorMinus = 0;
+	int contadorMayus = 0;
+	
+	for (int i = 0; i < contrasenya.length(); i++) {
+		  char desglose = contrasenya.charAt(i);
+		  if (Character.isDigit(desglose)) {
+			  contadorNumeros++;
+		  }else if (Character.isUpperCase(desglose)) {
+			  contadorMayus++;	  
+		  }else {
+			  contadorMinus++;
+		  }
+		 
+	}
+	if (contadorNumeros>6 && contadorMinus>1 && contadorMayus>2) {
+		this.fuerte = true;
+	}else {
+		this.fuerte = false;
+	}
+
+	}
+
 	//Imprimir Longitud + contraseña
 	@Override
 	public String toString() {
-		return "Password [longitud=" + longitud + ", contraseña=" + contrasenya + "]";
+		return "Password [longitud=" + longitud + ", contrasenya=" + contrasenya + ", fuerte=" + fuerte + "]";
 	}
+	
+	
+	
+	
+	
 
 
 
--- src/Test05/mainApp.java
+++ src/Test05/mainApp.java
@@ -1,10 +1,41 @@
 package Test05;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class mainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Password password = new Password();
 
+		password.contrasenyaAleatoria();
+		if (password.isFuerte()== false) {
+			System.out.println("La contraseña es débil");
+		}else {
+			System.out.println("La constraseña es fuerte");
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce la cantidad de contraseñas que quieres generar: ");
+		int cantidadPass = sc.nextInt();
+		
+		System.out.println("Introduce la longitud de la contraseña: ");
+		int longitudPass = sc.nextInt();
+		
+		Password passwordArray[] = new Password[cantidadPass];
+		for (int i = 0; i < cantidadPass; i++) {
+			Password pssw = new Password(longitudPass);
+			passwordArray[i] = pssw;
+			System.out.println(passwordArray[i]);	
+		}
+	
+		
+		
+	
+
+}
+
+	
 }
